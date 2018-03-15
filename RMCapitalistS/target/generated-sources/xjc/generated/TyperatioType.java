cfd371f8ecf3ff5e98c854646aa8cc862d461dd4
ChilanEli <yukseleli@gmail.com>
2018-03-15 18:25:58 +0100

    XSD
    Chemin XSD changé dans XML + nom serveur changé

--- RMCapitalistS/target/generated-sources/xjc/generated/TyperatioType.java
+++ RMCapitalistS/target/generated-sources/xjc/generated/TyperatioType.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.11 
 // Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2018.03.15 à 02:45:39 PM CET 
+// Généré le : 2018.03.15 à 06:22:29 PM CET 
 //
 
 
